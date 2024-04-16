397cee4042d4ba692c3dc3ef27332130cb42b340
Pedro Akira <40009054+pedroAkiraDanno@users.noreply.github.com>
2024-04-16 10:10:05 -0300

    Update IIS.ps1

--- IIS.ps1
+++ IIS.ps1
@@ -1,10 +1,10 @@
-✅Script for installing IIS:
-VM-WEB script
-Install IIS with Management Tools
+#✅Script for installing IIS:
+#VM-WEB script
+#Install IIS with Management Tools
 Add-WindowsFeature Web-Server -IncludeManagementTools
 
 Remove the iisstart page
 remove-item C:\inetpub\wwwroot\iisstart.htm
 
-Configure default pages
+#Configure default pages
 Add-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value "Live TFTEC APP GW -- $($env:computername)"
